1319b9d57df263d62646b0db23e67d13c281e18f
JosueSantosG <josue.santosgonzabay@upse.edu.ec>
2026-01-12 19:58:38 -0500

    Astro Configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://josuesantosg.github.io',
+  base: '/sassy-pet',
   vite: {
     plugins: [tailwindcss()]
   }
